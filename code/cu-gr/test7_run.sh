1bb487dcc961c40a20ce16afae55fe67591419a9
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2020-05-29 21:57:21 +0200

    Add A* to cu-gr

---
+++ code/cu-gr/test7_run.sh
@@ -0,0 +1,3 @@
+cd run/;
+./iccad19gr -lef ../toys/iccad2019c/ispd18_sample/ispd18_test7.input.lef -def ../toys/iccad2019c/ispd18_sample/ispd18_test7.input.def -output ispd18_test7.solution.guide -threads 20;
+cd ..;
